95b2a8f261af2516a324c52ba698545fa1366f15
Tobi Farinde <thetobifarinde@gmail.com>
2024-03-26 06:54:02 +0100

    submit id trigger

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const apikey = "90b3dbf58399096ad8f77887ad1818cc";
 const waetherDataEl = document .getElementById('weather-data')
 
 const cityInputEl = document.getElementById("city-input")
+
+const formEl = document.querySelector("form")
+
+formEl.addEventListener("submit", (event)=>{
+event.preventDefault();
+const cityValue = cityInputEl.value;
+console.log(cityValue);
+});
